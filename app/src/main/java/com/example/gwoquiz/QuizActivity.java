548ace32748c48cb79d28f1bef28d0298b6ae5b7
qristine <christina.vanesyan@gmail.com>
2020-11-04 23:40:07 +0400

    activity life circle with 2 excercise

--- app/src/main/java/com/example/gwoquiz/QuizActivity.java
+++ app/src/main/java/com/example/gwoquiz/QuizActivity.java
@@ -2,24 +2,23 @@ package com.example.gwoquiz;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String KEY_SCORE = "score";
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
     private Button prevButton;
     private TextView textView;
+    private int score;
     private int counter = 0;
     private Question[] questions = new Question[]{
             new Question(R.string.q1, true),
@@ -37,15 +36,21 @@ public class QuizActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             counter = savedInstanceState.getInt(KEY_INDEX, 0);
         }
-
-        Log.d(TAG, "Create");
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(KEY_SCORE, 0);
+        }
+        
 
         textView = (TextView) findViewById(R.id.textView);
-        updateQuestion();
+
         textView = findViewById(R.id.textView);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                trueButton.setEnabled(true);
+                Log.i(TAG, "true");
+                falseButton.setEnabled(true);
+                Log.i(TAG, "true");
                 counter = (counter + 1) % questions.length;
                 updateQuestion();
             }
@@ -54,7 +59,11 @@ public class QuizActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                trueButton.setEnabled(true);
+                Log.i(TAG, "true");
+                falseButton.setEnabled(true);
                 counter = (counter + 1) % questions.length;
+
                 updateQuestion();
             }
         });
@@ -70,7 +79,9 @@ public class QuizActivity extends AppCompatActivity {
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_LONG).show();
+                trueButton.setEnabled(false);
+                Log.i(TAG, "false");
+                falseButton.setEnabled(false);
                 checkAnswer(true);
                 counter = (counter + 1) % questions.length;
                 updateQuestion();
@@ -82,37 +93,64 @@ public class QuizActivity extends AppCompatActivity {
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_LONG).show();
+                trueButton.setEnabled(false);
+                Log.i(TAG, "false");
+                falseButton.setEnabled(false);
                 checkAnswer(false);
                 counter = (counter + 1) % questions.length;
                 updateQuestion();
+
             }
         });
+        updateQuestion();
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
+        Log.i(TAG, score + "");
         savedInstanceState.putInt(KEY_INDEX, counter);
+        savedInstanceState.putInt(KEY_SCORE, score);
     }
 
     private void updateQuestion() {
         int question = questions[counter].getQuestionId();
         textView.setText(question);
+        trueButton.setEnabled(true);
+        Log.i(TAG, "true");
+        falseButton.setEnabled(true);
+        /*
+         mFalseButton.setVisibility(View.VISIBLE);
+         mTrueButton.setVisibility(View.VISIBLE);
+         for (Integer i = 0; i < mQuestionAsked.size(); i++) {
+                if (mCurrentIndex == mQuestionAsked.get(i)) {
+                    mFalseButton.setVisibility(View.INVISIBLE);
+                    mTrueButton.setVisibility(View.INVISIBLE);
+                }
+            }
+   */
     }
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = questions[counter].isAnswer();
         int messageResId = 0;
+        int scoreID = 0;
         if (userPressedTrue == answerIsTrue) {
+            score++;
+
             messageResId = R.string.correct_toast;
+            scoreID = R.string.score;
         } else {
             messageResId = R.string.incorrect_toast;
+            scoreID = R.string.score;
         }
 
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-                .show();
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "" + counter);
+        if (counter == questions.length - 1) {
+            Toast.makeText(this, (score*100 / questions.length) + "%", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -137,7 +175,6 @@ public class QuizActivity extends AppCompatActivity {
     public void onDestroy() {
 
         super.onDestroy();
-        Log.d(TAG, "Destroy\n");
     }
 
 }
